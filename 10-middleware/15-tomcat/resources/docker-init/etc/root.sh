272e794eb82d6997eab4a694ef19b8bc0134d96c
trac-zh <trac-zh@users.noreply.github.com>
2021-03-28 14:01:05 +0800

    update: 1. remain /var/lib/tomcat9/webapps for more compatibility; 2. redirect log to /tmp, then use tail to show log

--- 10-middleware/15-tomcat/resources/docker-init/etc/root.sh
+++ 10-middleware/15-tomcat/resources/docker-init/etc/root.sh
@@ -13,4 +13,6 @@ export CATALINA_BASE=/var/lib/tomcat9
 export CATALINA_TMPDIR=/tmp
 export JAVA_OPTS=-Djava.awt.headless=true
 /usr/libexec/tomcat9/tomcat-update-policy.sh
-/usr/libexec/tomcat9/tomcat-start.sh
+
+nohup /usr/libexec/tomcat9/tomcat-start.sh 1 > /tmp/tomcat.log 2>&1 &
+tail -f /tmp/tomcat.log
